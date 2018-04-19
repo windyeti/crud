655b5d8f8d6eb8145143513f8f0025aefec5a5c6
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-04-19 14:14:35 +0300

    ready MongoDb

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const consolidate = require('consolidate');
-// const handlebars = require('handlebars');
 const path = require('path');
 const mongoose = require('mongoose');
 
 
-// const { Task } = require('./models');
 const { List } = require('./models');
 
 mongoose.connect('mongodb://localhost:27017/tasks', (err) => {
@@ -26,23 +24,28 @@ app.use(bodyParser.json());
 app.get('/', async (req, res) => {
   await List.find().then(result => {
     console.log('result', result);
-    res.render('tasks', result)
+    req.list = result;
+    res.render('tasks', req)
   })
 });
 
-// app.post('/tasks', async (req, res) => {
-//     const { nameTask } = req.body;
-//     await Task.create(nameTask);
-//     res.send({"ok" : "ok"});
-// });
-// app.put('/complete', async (req, res) => {
-//     await Task.complete(req.body.value);
-//     res.send({'response' : 'пометил выполненым'});
-// });
-// app.delete('/delete', async (req, res) => {
-//     await Task.delete(req.body.value);
-//     res.send({'response' : 'удалил'});
-// });
+app.post('/tasks', async (req, res) => {
+  const { nameTask } = req.body;
+  await new List({ name : nameTask, ready : false }).save();
+  // res.redirect('/');
+  res.send({"ok" : "ok"});
+});
+app.put('/complete', async (req, res) => {
+  const { value } = req.body;
+  await List.update( { _id : value }, { $set : { ready : true } });
+  // res.redirect('/');
+  res.send({'response' : 'пометил выполненым'});
+});
+app.delete('/delete', async (req, res) => {
+    const { value } = req.body;
+    await List.remove({ _id : value });
+    res.send({'response' : 'удалил'});
+});
 
 
 app.listen(8989);
