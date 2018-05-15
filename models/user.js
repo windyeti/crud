3887680a5438a0159bd3a1f1037466fea3a34b1a
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2018-05-15 23:10:20 +0300

    auth ready

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-  name : String,
+  username : String,
   password : String
 });
 
--- server.js
+++ server.js
@@ -12,46 +12,46 @@ mongoose.connect('mongodb://localhost:27017/tasks', (err) => {
 });
 
 const app = express();
+const passport = require('passport');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser());
 //
-app.use((req, res, next) => {
-  console.log('req.body', req.body);
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('req.body', req.body);
+//   next();
+// });
 //
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 const session = require('cookie-session');
+// app.use(session({ secret: 'anything' }));
 app.use(session({ keys : ['secret'] }));
 
 app.engine('hbs', consolidate.handlebars);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
-app.use(express.static(path.join(__dirname, "public")));
 
 // ------ подключение passport
 
-const passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
 const LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy((username, password, done) => {
-  console.log('username', username);
-  console.log('password', password);
   // .select('_id username fullname')
   // .lean() -- преобразует объект монгодб в обычный объект js
 
-  User.findOne({ username }).lean().then(user => {
+  User.findOne({ username }).then((user) => {
     if(user) {
       if(user.password === password) {
-        console.log('user befor', user);
-        delete user.password;
-        console.log('user after', user);
+        // console.log('user befor', user);
+        // delete user.password;
+        // console.log('user after', user);
         // done(null, {_id: new Object('5ae00be5a338a917c3d67f8e')});
         done(null, user);
       } else {
@@ -111,13 +111,10 @@ app.get('/logout', (req, res) => {
 // ------ конец подключения passport
 
 app.get('/', async (req, res) => {
-  await List.find().lean().then(result => {
-    console.log('result', result);
-    const nnnnn = req.user;
-    console.log('nnnnn',nnnnn);
-    // console.log('req.user.username', user.username);
+  await List.find().then((result) => {
+    console.log('req.user', req.user);
+    console.log('req.user.username', req.user.username);
     req.data = { list : result, user : req.user };
-    console.log('req.data', req.data);
     res.render('tasks', req.data)
   })
 });
